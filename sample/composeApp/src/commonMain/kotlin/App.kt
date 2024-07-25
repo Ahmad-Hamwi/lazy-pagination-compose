07a053bc349141ee492fbce56b8d2f92ff97fa65
Ahmad-Hamwi <ahmadhamwi999@gmail.com>
2024-07-26 00:52:59 +0400

    Tweaks sample UI

--- sample/composeApp/src/commonMain/kotlin/App.kt
+++ sample/composeApp/src/commonMain/kotlin/App.kt
@@ -1,11 +1,18 @@
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import io.github.ahmad_hamwi.compose.pagination.PaginatedLazyColumn
 import io.github.ahmad_hamwi.compose.pagination.rememberPaginationState
@@ -15,11 +22,31 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        Scaffold {
+        Scaffold(
+            topBar = { TopBar() }
+        ) {
             Content(
                 modifier = Modifier.padding(it)
             )
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    Column {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    "Lazy Pagination - Compose Multiplatform",
+                    fontWeight = FontWeight.Bold
+                )
+            },
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors()
+                .copy(containerColor = MaterialTheme.colors.surface)
+        )
+        HorizontalDivider()
     }
 }
 
--- sample/composeApp/src/commonMain/kotlin/MyItem.kt
+++ sample/composeApp/src/commonMain/kotlin/MyItem.kt
@@ -1,11 +1,10 @@
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,18 +13,17 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun Item(value: String) {
-    Card(
-        backgroundColor = MaterialTheme.colors.surface,
-        shape = RoundedCornerShape(10.dp),
-        elevation = 4.dp,
+    Box(
+        modifier = Modifier
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colors.primary,
+                shape = RoundedCornerShape(10.dp)
+            )
+            .fillMaxWidth()
+            .padding(vertical = 48.dp, horizontal = 16.dp),
+        contentAlignment = Alignment.Center
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 48.dp, horizontal = 16.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = value, fontWeight = FontWeight.Bold)
-        }
+        Text(text = value, fontWeight = FontWeight.Bold)
     }
 }

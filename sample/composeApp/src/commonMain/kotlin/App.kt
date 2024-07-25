117730fa927249bed41e72ed58e18f23cd16e705
Ahmad-Hamwi <ahmadhamwi999@gmail.com>
2024-07-26 02:41:48 +0400

    Adds swipe refresh

--- sample/composeApp/src/commonMain/kotlin/App.kt
+++ sample/composeApp/src/commonMain/kotlin/App.kt
@@ -1,4 +1,5 @@
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
@@ -10,8 +11,13 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import io.github.ahmad_hamwi.compose.pagination.PaginatedLazyColumn
@@ -52,6 +58,7 @@ fun TopBar() {
 
 val dataSource = DataSource()
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Content(modifier: Modifier = Modifier) {
     val paginationState = rememberPaginationState(
@@ -69,34 +76,54 @@ fun Content(modifier: Modifier = Modifier) {
         }
     )
 
-    PaginatedLazyColumn(
-        modifier = modifier,
-        paginationState = paginationState,
-        firstPageProgressIndicator = { FirstPageProgressIndicator() },
-        newPageProgressIndicator = { NewPageProgressIndicator() },
-        firstPageErrorIndicator = { e ->
-            FirstPageErrorIndicator(
-                exception = e,
-                onRetryClicked = {
-                    paginationState.retryLastFailedRequest()
-                }
-            )
-        },
-        newPageErrorIndicator = { e ->
-            NewPageErrorIndicator(
-                exception = e,
-                onRetryClicked = {
-                    paginationState.retryLastFailedRequest()
-                }
-            )
-        },
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+    val pullToRefreshState = rememberPullToRefreshState()
+
+    if (pullToRefreshState.isRefreshing) {
+        LaunchedEffect(true) {
+            paginationState.refresh()
+            pullToRefreshState.endRefresh()
+        }
+    }
+
+    Box(
+        modifier = modifier.nestedScroll(pullToRefreshState.nestedScrollConnection)
     ) {
-        itemsIndexed(
-            paginationState.allItems,
-        ) { _, item ->
-            Item(value = item)
+        PaginatedLazyColumn(
+            modifier = modifier,
+            paginationState = paginationState,
+            firstPageProgressIndicator = { FirstPageProgressIndicator() },
+            newPageProgressIndicator = { NewPageProgressIndicator() },
+            firstPageErrorIndicator = { e ->
+                FirstPageErrorIndicator(
+                    exception = e,
+                    onRetryClicked = {
+                        paginationState.retryLastFailedRequest()
+                    }
+                )
+            },
+            newPageErrorIndicator = { e ->
+                NewPageErrorIndicator(
+                    exception = e,
+                    onRetryClicked = {
+                        paginationState.retryLastFailedRequest()
+                    }
+                )
+            },
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            itemsIndexed(
+                paginationState.allItems,
+            ) { _, item ->
+                Item(value = item)
+            }
+        }
+
+        if (pullToRefreshState.progress > 0) {
+            PullToRefreshContainer(
+                state = pullToRefreshState,
+                modifier = Modifier.align(Alignment.TopCenter)
+            )
         }
     }
 }

b056e504a1e079b724355b32aedc6564eb4def03
Ahmad-Hamwi <ahmadhamwi999@gmail.com>
2024-07-26 00:51:14 +0400

    Fixes system bar colors on Android

--- sample/composeApp/src/androidMain/kotlin/io/github/ahmad_hamwi/MainActivity.kt
+++ sample/composeApp/src/androidMain/kotlin/io/github/ahmad_hamwi/MainActivity.kt
@@ -1,16 +1,46 @@
 package io.github.ahmad_hamwi
 
 import App
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+            ChangeSystemBarColors()
+
             App()
+        }
+    }
+}
+
+@Composable
+fun ChangeSystemBarColors(
+    statusBarColor: Color = MaterialTheme.colors.surface,
+    navigationBarColor: Color = MaterialTheme.colors.background,
+) {
+    val view = LocalView.current
+    val darkTheme = isSystemInDarkTheme()
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = statusBarColor.toArgb()
+            window.navigationBarColor = navigationBarColor.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !darkTheme
         }
     }
 }

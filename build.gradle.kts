ace2248febe0f5aee82050ca19cb7a52a27ab80d
Ahmad-Hamwi <ahmadhamwi999@gmail.com>
2024-07-29 21:56:15 +0400

    Adds missing plugin declaration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("root.publishing")
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
     alias(libs.plugins.androidApplication) apply false
